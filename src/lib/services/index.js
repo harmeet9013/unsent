70be076f3728c82bb6db72521b8db12780fd38d8
harmeet9013 <evildoremon@gmail.com>
2024-11-10 11:49:40 +0530

    reworked axios to fetch api calls

--- src/lib/services/index.js
+++ src/lib/services/index.js
@@ -1,20 +1,43 @@
-import { axios } from "../axios";
+import { SERVER_URL } from "@/config";
+
+const generateRequest = async (
+    url,
+    method = "GET",
+    params = {},
+    body = {},
+    headers = {}
+) => {
+    const apiParams = new URLSearchParams(params);
+    const apiUrl = `${
+        typeof SERVER_URL !== "undefined" ? SERVER_URL : ""
+    }${url}?${apiParams}`;
+
+    const raw_response = await fetch(apiUrl, {
+        method: method || "GET",
+        cache: "no-store",
+        headers: {
+            ...headers,
+        },
+        ...(["POST", "PUT"]?.includes(method)
+            ? {
+                  body: JSON.stringify(body),
+              }
+            : {}),
+    });
+
+    if (raw_response?.status === 200) {
+        return await raw_response.json();
+    } else {
+        return {
+            status: false,
+            message: raw_response?.statusText || "Unexpected Error",
+            data: [],
+        };
+    }
+};
 
 export const GET_REQUEST = async (url, params = {}) => {
-    return await axios
-        .get(url, {
-            params,
-        })
-        .then((response) => {
-            return response?.data;
-        })
-        .catch((error) => {
-            return {
-                status: false,
-                message: error?.message || JSON.stringify(error),
-                data: false,
-            };
-        });
+    return await generateRequest(url, "GET", params);
 };
 
 export const POST_REQUEST = async (
@@ -23,51 +46,13 @@ export const POST_REQUEST = async (
     params = {},
     headers = {}
 ) => {
-    return await axios
-        .post(url, data, {
-            params,
-            headers: {
-                ...headers,
-            },
-        })
-        .then((response) => {
-            return response?.data;
-        })
-        .catch((error) => {
-            return {
-                status: false,
-                message: error?.message || JSON.stringify(error),
-                data: false,
-            };
-        });
+    return await generateRequest(url, "POST", params, data, headers);
 };
 
 export const PUT_REQUEST = async (url, data = {}, params = {}) => {
-    return await axios
-        .put(url, data, { params })
-        .then((response) => {
-            return response?.data;
-        })
-        .catch((error) => {
-            return {
-                status: false,
-                message: error?.message || JSON.stringify(error),
-                data: false,
-            };
-        });
+    return await generateRequest(url, "PUT", params, data);
 };
 
 export const DELETE_REQUEST = async (url, params = {}) => {
-    return await axios
-        .delete(url, { params })
-        .then((response) => {
-            return response?.data;
-        })
-        .catch((error) => {
-            return {
-                status: false,
-                message: error?.message || JSON.stringify(error),
-                data: false,
-            };
-        });
+    return await generateRequest(url, "DELETE", params);
 };
